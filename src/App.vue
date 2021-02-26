fdcba80ecc3fcebda8762f9b6f771326aa3ff4c9
wangbo <wangbo@xbcx.com.cn>
2021-02-26 16:49:00 +0800

    'newfile'

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <h1>TS study</h1>
     <router-view/>
   </div>
 </template>
